ce50ca12cdb3d1938757fbea5c538d212b5eab37
Matthew Dawson <matt.dawson@fluxfederation.com>
2020-09-28 20:46:15 +1300

    Create bootable OS

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Based on https://os.phil-opp.com
 
-Compile with `cargo build --target x86_64-lose_thos.json`
+Compile with `cargo build` (default target is `x86_64-lose_thos.json`)
+
+Run with `cargo run`  (Require QEMU)
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,24 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+// extern crate rlibc;
+
+static HELLO: &[u8] = b"Hello World!";
 
 // Defualt entry point - name mangling changes the signature of this method so some hash
 // Linker requires a function called `_start` as entry point
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-        loop {}
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
 }
 
 // Called on panc... required when excluding std library
